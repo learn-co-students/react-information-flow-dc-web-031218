bb3256e2bc8caf737c5607dafcaf538d61afea75
rpyoungdale <youngdale95@gmail.com>
2018-05-21 09:21:41 -0400

    Done.

--- src/Tier2.js
+++ src/Tier2.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react'
-import { getReducedColor } from './randomColorGenerator.js'
-import Tier3 from './Tier3'
+import React, { Component } from "react";
+import { getReducedColor } from "./randomColorGenerator.js";
+import Tier3 from "./Tier3";
 
+const Tier2 = props => {
+  return (
+    <div
+      id={props.id}
+      className="tier2"
+      style={{ backgroundColor: props.tier2Color, color: props.tier2Color }}
+    >
+      <Tier3 tier3Color={props.tier3Color} />
+      <Tier3 tier3Color={props.tier3Color} />
+    </div>
+  );
+};
 
-export default class Tier2 extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      childColor: getReducedColor(this.props.color),
-    }
-  }
-
-  render() {
-    // hard coded color values have been added below, though they won't be
-    // present in our solution. What should they be replaced with?
-    return (
-      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
-      </div>
-    )
-  }
-}
+export default Tier2;
--- src/Tier3.js
+++ src/Tier3.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-
-const Tier3 = (props) => (
+const Tier3 = props => (
   // this component does not need to change (but feel free to change however you like!)
-  <div className="tier3" onClick={props.handleChildClick} style={{backgroundColor: props.color, color: props.color}}></div>
-)
+  <div
+    className="tier3"
+    style={{ backgroundColor: props.tier3Color, color: props.tier3Color }}
+  />
+);
 
-export default Tier3
+export default Tier3;
